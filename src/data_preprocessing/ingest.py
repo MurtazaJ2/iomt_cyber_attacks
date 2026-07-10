cd1876ba7f8f07b9726d31d840540693126aa9a6
reeya thanki <reeya.thanki@moschip.com>
2026-07-10 13:06:23 +0530

    test: user reverted ingest.py to trigger workflow

--- src/data_preprocessing/ingest.py
+++ src/data_preprocessing/ingest.py
@@ -1,6 +1,12 @@
-def print_numbers(n: int) -> None:
-    """Print numbers from 1 to n"""
-    for i in range(1, n+1):
-        print(i)
-
-print_numbers(12)
+unused_var = 10
+import math
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
