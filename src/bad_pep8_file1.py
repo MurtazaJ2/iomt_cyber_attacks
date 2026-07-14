191c671ad8b64bf0970792eb9c96e898013a9129
reeya thanki <reeya.thanki@moschip.com>
2026-07-14 09:54:29 +0530

    test: add two bad PEP8 files

---
+++ src/bad_pep8_file1.py
@@ -0,0 +1,7 @@
+import os ,sys, time
+def ThisIsBadCode( a,b,c):
+ x=a+b
+ y= b+ c
+ if x==y:
+  print("equals")
+ return x+y
---
+++ src/bad_pep8_file2.py
@@ -0,0 +1,4 @@
+def anotherBAD_function():
+ x= [ 1,2 ,3]
+ for i in x: print(i)
+ return x
