e6d21a1277bb5f10264e24f7c7f6a3cc06ca5de6
reeya thanki <reeya.thanki@moschip.com>
2026-08-07 12:21:17 +0530

    Minor tweak to trigger PR agent again

--- src/bad_pep8.py
+++ src/bad_pep8.py
@@ -5,5 +5,5 @@ def badCamelCaseFunction( A,B ):
   print("Hello") # Unnecessary generic print
   unused_var = 100
   if A > B:
-    print( "A is greater" ) # Solitary if without else
+    print( "A is greater" ) # Solitary if without 
   return A+B
